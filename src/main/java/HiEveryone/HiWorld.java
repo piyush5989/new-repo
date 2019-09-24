995c604ddcd9a8b3ef4b8b832bbede8fce7bf1af
Piyush Yadav <31996687+piyush5989@users.noreply.github.com>
2019-09-24 16:20:49 +0530

    SWCDDTRAIN-734: subtask in progress

--- src/main/java/HiEveryone/HiWorld.java
+++ src/main/java/HiEveryone/HiWorld.java
@@ -3,6 +3,7 @@ package HiEveryone;
 public class HiWorld {
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHi());
+		
+		System.out.println("msg: "+greeter.sayHi());
 	}
 }
