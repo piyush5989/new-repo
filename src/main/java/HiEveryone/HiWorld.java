85b56127fce63fa34297af57814bedf9385e1f4d
Piyush Yadav <31996687+piyush5989@users.noreply.github.com>
2020-03-03 11:52:43 +0530

    Update HiWorld.java

--- src/main/java/HiEveryone/HiWorld.java
+++ src/main/java/HiEveryone/HiWorld.java
@@ -4,6 +4,6 @@ public class HiWorld {
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
 		
-		System.out.println("Message- "+greeter.sayHi());
+		System.out.println("Message: "+greeter.sayHi());
 	}
 }
